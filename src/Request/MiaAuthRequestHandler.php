5576f8b51f6e859a45aa5212f3ce21ec450a7875
Matias Camiletti <matias.camiletti@gmail.com>
2018-06-18 20:02:41 -0300

    Agregado request para que sea sencillo obtener el usuario logueado.

---
+++ src/Request/MiaAuthRequestHandler.php
@@ -0,0 +1,20 @@
+<?php namespace Mobileia\Expressive\Auth\Request;
+
+
+/**
+ * Description of MiaAuthRequestHandler
+ *
+ * @author matiascamiletti
+ */
+class MiaAuthRequestHandler extends \Mobileia\Expressive\Request\MiaRequestHandler
+{
+    /**
+     * Obtiene usuario logueado
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @return \Mobileia\Expressive\Auth\Model\MIAUser
+     */
+    protected function getUser(\Psr\Http\Message\ServerRequestInterface $request)
+    {
+        return $request->getAttribute(\Mobileia\Expressive\Auth\Model\MIAUser::class);
+    }
+}
