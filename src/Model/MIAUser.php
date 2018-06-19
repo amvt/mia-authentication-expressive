0cb2e05ed559ed967b2bdb1f353339c540437f2b
Matias Camiletti <matias.camiletti@gmail.com>
2018-06-19 16:08:01 -0300

    Ocultado campo "deleted"

--- src/Model/MIAUser.php
+++ src/Model/MIAUser.php
@@ -3,4 +3,9 @@
 class MIAUser extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'mia_user';
+    /**
+     * Campos que se ocultan al obtener los registros
+     * @var array
+     */
+    protected $hidden = ['deleted'];
 }
