1c8768b7bf52c50061da06b35528f8b64391ee9e
Matias Camiletti <matias.camiletti@gmail.com>
2019-05-11 01:29:53 -0300

    Agregado middleware base.

---
+++ src/Middleware/MiaAuthMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mobileia\Expressive\Auth\Middleware;
+/**
+ * Description of MiaAuthMiddleware
+ *
+ * @author matiascamiletti
+ */
+class MiaAuthMiddleware extends \Mobileia\Expressive\Middleware\MiaBaseMiddleware
+{
+    /**
+     * Obtiene usuario logueado
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @return \Mobileia\Expressive\Auth\Model\MIAUser
+     */
+    protected function getUser(\Psr\Http\Message\ServerRequestInterface $request)
+    {
+        return $request->getAttribute(\Mobileia\Expressive\Auth\Model\MIAUser::class);
+    }
+}
