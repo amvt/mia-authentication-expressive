45928bdf64e7ea6ae16884957c346c3d5b02a3bb
Matias Camiletti <matias.camiletti@gmail.com>
2019-08-24 16:23:39 -0300

    Actualizado servicio para obtener el perfil del usuario logueado.

--- src/Handler/FetchProfileHandler.php
+++ src/Handler/FetchProfileHandler.php
@@ -7,22 +7,12 @@ namespace Mobileia\Expressive\Auth\Handler;
  *
  * @author matiascamiletti
  */
-class FetchProfileHandler extends \Mobileia\Expressive\Request\MiaRequestHandler
+class FetchProfileHandler extends \Mobileia\Expressive\Auth\Request\MiaAuthRequestHandler
 {
     public function handle(\Psr\Http\Message\ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
     {
-        // Obtener cuenta seleccionada
-        $miaId = $this->getParam($request, 'id', 0);
-        // Verificamos que se haya enviado los datos del usuario
-        if($miaId <= 0){
-            return new Mobileia\Expressive\Diactoros\MiaJsonErrorResponse(-1, 'No se ha enviado el ID');
-        }
-        // Buscar por el MIA_ID
-        $user = \Mobileia\Expressive\Auth\Model\MIAUser::where('mia_id', $miaId)->first();
-        // Verificamos si ya existe este MIA_ID
-        if($user === null){
-            return new Mobileia\Expressive\Diactoros\MiaJsonErrorResponse(-1, 'No se ha encontrado el usuario');
-        }
+        // Obtener usuario
+        $user = $this->getUser($request);
         // Devolvemos datos del usuario
         return new \Mobileia\Expressive\Diactoros\MiaJsonResponse($user->toArray());
     }
