9cd1017c2898ca825b211dc0b4ad333e211edef4
Matias Camiletti <matias.camiletti@gmail.com>
2020-03-01 16:07:56 -0300

    Estructura de servicio de registro interno.

---
+++ src/Handler/RegisterInternalHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mobileia\Expressive\Auth\Handler;
+
+/**
+ * Description of RegisterInternalHandler
+ *
+ * @author matiascamiletti
+ */
+class RegisterInternalHandler extends \Mobileia\Expressive\Request\MiaRequestHandler
+{
+    public function handle(\Psr\Http\Message\ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
+    {
+        // Obtener parametros obligatorios
+        $email = $this->getParam($request, 'email', '');
+        $password = $this->getParam($request, 'password', '');
+        // Verificar si ya existe la cuenta
+        $account = \Mobileia\Expressive\Auth\Model\MIAUser::where('email', $email)->first();
+        if($account !== null){
+            return new Mobileia\Expressive\Diactoros\MiaJsonErrorResponse(-1, 'Este email ya se encuentra registrado');
+        }
+        // Creamos cuenta
+        $account = new \Mobileia\Expressive\Auth\Model\MIAUser();
+        $account->mia_id = 0;
+        $account->firstname = $this->getParam($request, 'firstname', '');
+        $account->lastname = $this->getParam($request, '', '');
+        $account->email = $email;
+        $account->phone = $this->getParam($request, 'phone', '');
+        $account->photo = $this->getParam($request, 'photo', '');
+        $account->password = \Mobileia\Expressive\Auth\Model\MIAUser::encryptPassword($password);
+        $account->role = \Mobileia\Expressive\Auth\Model\MIAUser::ROLE_GENERAL;
+        $account->save();
+        // Devolvemos datos del usuario
+        return new \Mobileia\Expressive\Diactoros\MiaJsonResponse($account->toArray());
+    }
+}
--- src/Model/MIAUser.php
+++ src/Model/MIAUser.php
@@ -10,5 +10,29 @@ class MIAUser extends \Illuminate\Database\Eloquent\Model
      * Campos que se ocultan al obtener los registros
      * @var array
      */
-    protected $hidden = ['deleted'];
+    protected $hidden = ['deleted', 'password'];
+    
+    /**
+     * 
+     * @param string $password
+     * @return string
+     */
+    public static function encryptPassword($password)
+    {
+        $bcrypt = new \Zend\Crypt\Password\Bcrypt();
+        $bcrypt->setCost(10);
+        return $bcrypt->create($password);
+    }
+    /**
+     * Valida si el password es correcto
+     * @param string $password
+     * @param string $hash
+     * @return boolean
+     */
+    public static function verifyPassword($password, $hash)
+    {
+        $bcrypt = new \Zend\Crypt\Password\Bcrypt();
+        $bcrypt->setCost(10);
+        return $bcrypt->verify($password, $hash);
+    }
 }
