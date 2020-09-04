86d25f091457e0093f92ee29e2c5f8ac4d84cfe5
Matias Camiletti <matias.camiletti@gmail.com>
2020-09-04 15:20:33 -0300

    Update email.

--- src/Handler/UpdateProfileHandler.php
+++ src/Handler/UpdateProfileHandler.php
@@ -40,6 +40,7 @@ class UpdateProfileHandler extends \Mobileia\Expressive\Auth\Request\MiaAuthRequ
             if(\Mobileia\Expressive\Auth\Model\MIAUser::where('email', $newEmail)->first() !== null){
                 return new \Mobileia\Expressive\Diactoros\MiaJsonErrorResponse(-3, 'El nuevo email ya existe!');
             }
+            $item->email = $newEmail;
         }else{
             $item->email = $newEmail;
         }
